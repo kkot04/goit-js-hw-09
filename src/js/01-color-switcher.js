11e80a2b394f58afbdbabf875656c35fe7b70340
kkot04 <144848298+kkot04@users.noreply.github.com>
2023-11-14 13:51:47 +0200

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const contentWrapper = document.querySelector('.content-wrapper');
+const startBtn = contentWrapper.querySelector('button[data-start]');
+const stopBtn = contentWrapper.querySelector('button[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let timerColor;
+startBtn.addEventListener('click', () => {
+  timerColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled', false);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerColor);
+  startBtn.removeAttribute('disabled', true);
+  stopBtn.setAttribute('disabled', true);
+});
